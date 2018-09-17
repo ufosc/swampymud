3170efac4dba22af29a40bc6ce824c5dc26fb9b5
xeranimus <nobodycaresaboutspam@gmail.com>
2018-09-17 18:26:09 -0400

    added test items to ExampleClass.py

--- scripts/ExampleClass.py
+++ scripts/ExampleClass.py
@@ -1,8 +1,52 @@
 from character import Character
+import item
 
 class ExampleClass(Character):
-    def cmd_echo(self, args):
+    equip_slots = ["Head", "Torso", "Feet"]
+
+    def cmd_echo(self, *args):
         '''Echoes the provided phrase back.
         usage: example [phrase]
         '''
         self.message(args)
+    
+    def cmd_gimmie(self, *args):
+        self.inv += Necklace()
+        self.inv += BlueNecklace()
+        self.inv += RedNecklace()
+        self.inv += Pants()
+        self.inv += RedPants()
+        self.inv += Tentacle()
+        self.inv += Cuirass()
+
+class Necklace(item.EquippableBase):
+    target = item.EquipTarget("Head")
+    def __init__(self):
+        self.eqc = 0
+        self.uqc = 0
+
+    def equip(self, character):
+        print("%s equipped %s! %s" % (character, self.name, self.eqc))
+        self.eqc += 1
+
+    def unequip(self, character):
+        print("%s unequipped %s! %s" % (character, self.name, self.uqc))
+        self.uqc += 1
+
+class BlueNecklace(Necklace):
+    meme = ""
+
+class RedNecklace(Necklace):
+    meme = ""
+
+class Pants(Necklace):
+    target = item.EquipTarget("Feet")
+
+class RedPants(Necklace):
+    target = item.EquipTarget("Feet")
+
+class Tentacle(Necklace):
+    target = item.EquipTarget("Tentacle")
+
+class Cuirass(Necklace):
+    target = item.EquipTarget("Torso")
