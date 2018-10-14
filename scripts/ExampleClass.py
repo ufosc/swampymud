913356fbc100cb042e5d35175165f50018d44652
gageowe <gageowe@gmail.com>
2018-10-13 22:36:49 -0400

    Tweaked example class

--- scripts/ExampleClass.py
+++ scripts/ExampleClass.py
@@ -6,3 +6,10 @@ class ExampleClass(Character):
         usage: example [phrase]
         '''
         self.message(args)
+        
+    def cmd_me(self, args):
+        '''Illustrate an action of a character
+		usage me [phrase]
+        '''
+        self.location.message_chars("*%s %s" % (self,args))
+        
