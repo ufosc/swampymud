bf257db751448b5a8ca0b0f943bb7217bc5184c5
pankeleo <leonardoenriquetorres1@gmail.com>
2019-03-20 15:44:41 -0400

    Fixed build error with Sword Of Destiny

--- scripts/SwordOfDestiny.py
+++ scripts/SwordOfDestiny.py
@@ -1,6 +1,16 @@
 import item
 
-class SwordOfDestiny(item.Equippable):
+class SwordOfDestiny(item.EquippableBase):
     _item_name = "Sword Of Destiny"
     target = item.EquipTarget("Hands")
-#    def equip
+    def __init__(self):
+        self.eqc = 0
+        self.uqc = 0
+
+    def equip(self, character):
+        print("%s equipped %s! %s" % (character, self.name, self.eqc))
+        self.eqc +=1
+
+    def unequip(self, character):
+        print("%s unequipped %s! %s" % (character, self.name, self.uqc))
+        self.uqc += 1
