b73eae4f9d03bfec1b968670a4d33cc5d7904ec2
xeranimus <nobodycaresaboutspam@gmail.com>
2019-04-15 13:23:59 -0400

    added EntityCommand class and decorators

--- entity.py
+++ entity.py
@@ -7,11 +7,38 @@ class EntityMeta(type):
     def __init__(self, cls, namespace, bases):
         if "name" not in namespace:
             self.name = camel_to_space(cls)
+        # self._instances = []
+        # self._nextid = 0
 
+class EntityCommand:
+    def __init__(self, command, perms=None):
+        self.perms = perms
+        if perms is None:
+            self.perms = character.CharFilter("blacklist")
+        self.command = command
+    
+    def __call__(self, *args, **kwargs):
+        return self.command(*args, **kwargs)
+    
+    def __repr__(self):
+        return "EntityCommand(%r, %r)" % (self.command, self.perms)
+
+def filtered_command(perms):
+    '''decorator for methods with CharFilters'''
+    def inner(func):
+        return EntityCommand(func, perms)
+    return inner
+
+def entity_command(func):
+    '''decorator for methods without CharFilters'''
+    return EntityCommand(func)
 
 class Entity(metaclass=EntityMeta):
-    pass
+    def __init__(self, proper_name=None):
+        self.proper_name = proper_name
 
 
 class DroppedItem(Entity):
-    pass
+    @entity_command
+    def pickup(self, player):
+        pass
