586859736bef476b01252aa5c21a259fdaee49d8
xeranimus <nobodycaresaboutspam@gmail.com>
2019-04-15 13:59:48 -0400

    stored references to each entity inside the Entity Class

--- entity.py
+++ entity.py
@@ -7,8 +7,17 @@ class EntityMeta(type):
     def __init__(self, cls, namespace, bases):
         if "name" not in namespace:
             self.name = camel_to_space(cls)
-        # self._instances = []
-        # self._nextid = 0
+        self._instances = {}
+        self._nextid = 0
+    
+    def __getitem__(self, key):
+        return self._instances[key]
+    
+    def __setitem__(self, key, item):
+        self._instances[key] = item
+    
+    def __delitem__(self, key):
+        del self._instances[key]
 
 class EntityCommand:
     def __init__(self, command, perms=None):
@@ -35,10 +44,28 @@ def entity_command(func):
 
 class Entity(metaclass=EntityMeta):
     def __init__(self, proper_name=None):
+        self.location = None
         self.proper_name = proper_name
+        self._id = self._nextid
+        self._nextid += 1
+        self._instances[self._id] = self
+    
+    def __repr__(self):
+        return "%s[%i]" % (type(self).__name__, self._id)
+    
+    def __str__(self):
+        if self.proper_name is not None:
+            return self.proper_name
+        else:
+            return repr(self)
 
 
 class DroppedItem(Entity):
+    def __init__(self, item):
+        super().__init__("Dropped " + str(item))
+        self._item = item
+
     @entity_command
-    def pickup(self, player):
-        pass
+    def pickup(self, char):
+        char.inv
+        self.location
