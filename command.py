ad1f4b1c8ee066a558a0c70788be4f7e1a067ca1
xeranimus <nobodycaresaboutspam@gmail.com>
2019-04-16 21:05:08 -0400

    allowed command shadowing in command.py

--- command.py
+++ command.py
@@ -1,111 +1,153 @@
 '''module containing the CommandDict class'''
+from util.shadowdict import ShadowDict
 
 class Command:
+    '''class representing a Command
+    a Command has 3 important values:
+        - a name that will represent the command
+        - a function that the comman will call
+        - a type_name that is used for sorting the help menu
+    Optionally, a Command may also have a source
+    (this source is used if the function is an unbound method)
+    '''
+
     def __init__(self, name, func, type_name, source=None):
         self.name = name
         self._func = func
         self.type_name = type_name
         self.source = source
-    
+
     def __call__(self, *args, **kwargs):
-        if self.source:
-            return func(source, *args, **kwargs)
+        if self.source is not None:
+            return self._func(self.source, *args, **kwargs)
         else:
-            return func(*args, **kwargs)
-    
+            return self._func(*args, **kwargs)
+
+    def __repr__(self):
+        return "Command%r" % ((self.name, self._func, self.type_name, self.source),)
+
     def __str__(self):
         return self.name
 
-
+# TODO: improve the help menu to make it more efficient
+# options include adding indicies for CommandTypes
+# or simply caching the help menu
 class CommandDict:
-
     def __init__(self, cmds=[]):
-        self._commands = {}
+        self._commands = ShadowDict()
         self._command_names = {}
-        self._types = {}
         for cmd in cmds:
             self.add_cmd(cmd)
 
     def add_cmd(self, cmd, name=None):
+        '''add cmd to the dict with name
+        if 'name' is not provided, the builtin name
+        of the command is used'''
         if name is None:
             name = str(cmd)
         self._commands[name] = cmd
         self._command_names[cmd] = name
-        if cmd.type_name not in self._types:
-            self._types[cmd.type_name] = []
-        self._types[cmd.type_name].append(cmd)
-    
+
     def remove_cmd(self, cmd):
         '''provide a command to remove from the dict'''
         name = self._command_names[cmd]
         type_name = cmd.type_name
-        self._types[type_name].remove(cmd)
-        if not self._types[type_name]:
-            del self._types[type_name]
-        del self._commands[name]
+        self._commands.remove_value(name, cmd)
         del self._command_names[cmd]
-    
+
+    #TODO: document shadowing behavior
     def remove_name(self, name):
         '''provide a name to remove from the dict'''
         cmd = self._commands[name]
         type_name = cmd.type_name
-        self._types[type_name].remove(cmd)
-        if not self._types[type_name]:
-            del self._types[type_name]
         del self._commands[name]
         del self._command_names[cmd]
-    
+
     def change_name(self, current, new_name):
         '''change the name associated with a command'''
         cmd = self._commands[current]
         del self._commands[current]
         self._commands[new_name] = cmd
         self._command_names[cmd] = new_name
-        
+
     def get_cmd(self, name):
         '''get a cmd by providing its name'''
         return self._commands[name]
-    
+
     def get_name(self, cmd):
         '''get a name by providing the cmd'''
         return self._command_names[cmd]
-        
+
     def has_name(self, name):
-        '''returns true if cmd with name 'name' exists'''
+        '''returns true if 'name' is in use'''
         return name in self._commands
-    
+
     def has_cmd(self, cmd):
-        '''returns true if cmd with name 'name' exists'''
+        '''returns true if 'cmd' exists in the dict'''
         return cmd in self._command_names
 
+    def iter_cmds(self):
+        for cmd in self._command_names:
+            yield cmd
+
     def help(self, width=30):
+        '''produce a formatted help menu with width [width]'''
         output = ""
-        for type, cmds in self._types.items():
-            output += "{0:-^{width}}\n".format(type, width=width)
+
+        # TODO: improve this sorting process
+        types = {}
+        # create a type dictionary
+        for name, cmd in self._commands.items():
+            if cmd.type_name not in types:
+                types[cmd.type_name] = []
+            types[cmd.type_name].append(name)
+        typelist = list(types.keys())
+
+        # sorting through the typelist to ensure a reliable ordering
+        # TODO: find a better way of doing this
+        typelist.sort()
+        print(typelist)
+        if "Player" in typelist:
+            typelist.remove("Player")
+            typelist.insert(0, "Player")
+            print(typelist)
+        if "Equipped" in typelist:
+            typelist.remove("Equipped")
+            typelist.append("Equipped")
+        if "Environmental" in typelist:
+            typelist.remove("Environmental")
+            typelist.append("Environmental")
+        for typ in typelist:
+            names = types[typ]
+            output += "{0:-^{width}}\n".format(typ + " Commands", width=width)
             length = 0
-            maxlength = 0
-            names = []
-            for cmd in cmds:
-                name = self._command_names[cmd]
+            name_row = []
+            for name in names:
                 length += len(name) + 1
                 if length >= width - 2:
                     length = len(name)
-                    output += " " +  " ".join(names) + " \n"
-                    names = []
-                    maxlength = 0
-                names.append(name)
-            output += " " +  " ".join(names) + " \n"
+                    output += " " +  " ".join(name_row) + " \n"
+                    name_row = []
+                name_row.append(name)
+            output += " " +  " ".join(name_row) + " \n"
         return output
 
-def dummy_command(name, type_name):
-    return Command(name, lambda x: x, type_name)
-m = dummy_command("make", "BUILDING")
-c = dummy_command("compile", "BUILDING")
-d = dummy_command("decant", "ALCHEMY")
-b = dummy_command("brew", "ALCHEMY")
-cd = CommandDict()
-cd.add_cmd(m)
-cd.add_cmd(c)
-cd.add_cmd(d)
-cd.add_cmd(b)
-print(cd.help())
+
+# def dummy_command(name, type_name):
+#     return Command(name, lambda x: x, type_name)
+# y = dummy_command("say", "Player")
+# m = dummy_command("make", "Paladin")
+# c = dummy_command("compile", "Paladin")
+# d = dummy_command("decant", "Alchemist")
+# b = dummy_command("brew", "Alchemist")
+# p = dummy_command("pull", "Environmental")
+# s = dummy_command("swing", "Equipped")
+# x = dummy_command("brew", "Environmental")
+# cd = CommandDict()
+# cd.add_cmd(m)
+# cd.add_cmd(c)
+# cd.add_cmd(d)
+# cd.add_cmd(b)
+# print(cd.help())
+# cd.add_cmd(y)
+# print(cd.help())
