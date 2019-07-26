09758c7bdd559d7d4295694818dc36b2b26611e3
xeranimus <nobodycaresaboutspam@gmail.com>
2019-07-26 19:59:20 -0400

    added ItemStack and added basic new inventory

--- inventory.py
+++ inventory.py
@@ -1,6 +1,118 @@
-'''defining the inventory module'''
+'''this module contains the Inventory, the class for storing all items,
+and ItemStack, a class for efficiently storing items of the same type
+'''
+from collections import defaultdict
 
-#TODO: make internal implementation faster and more elegant
+def matching_subset(main, sub):
+    '''check that all the keys in a dictionary are in sub and agree with main
+Example:
+    main = {"a": 3, "b": 4, "c": 10}
+    sub1 = {"a": 3, "c": 10}
+    sub2 = {"a": 3, "foo": 5}
+    sub3 = {"a": 0, "b": 1}
+sub1 is a "matching subset" of main since all its keys match with main
+sub2 is not a "matching subset" of main since the field "foo" is not in main
+sub3 is not a "matching subset" since sub3["a"] = 0 but main["a"] == 3
+'''
+    # check that all keys of sub are in main
+    main_keys = set(main)
+    sub_keys = set(sub)
+    if not sub_keys.issubset(main_keys):
+        return False
+    # check that all values of sub match with main
+    for key in sub_keys:
+        if main[key] != sub[key]:
+            return False
+    return True
+
+
+class ItemStack:
+    def __init__(self, item_type, amount, data=None):
+        '''create a new ItemStack with Item class [item_type], integer [amount]
+Optionally, you can provide [data], where [data] is compatible with the .load
+method provided by [item_type]
+'''
+        # for sake of memory, store None instead of empty dict
+        if data == {}:
+            data = None
+        self._type = item_type
+        self._amount = amount
+        self._data = data
+
+    @property
+    def amount(self):
+        '''returns amount of items in this stack'''
+        return self._amount
+
+    @amount.setter
+    def amount(self, new_amt):
+        '''set the amount of items in this stack to ['''
+        if not isinstance(new_amt, int):
+            raise TypeError("Problem setting ItemStack amount:\
+                              expected type int, received %s" % type(new_amt))
+        if new_amt < 0:
+            raise ValueError("Problem setting ItemStack amount:\
+                              expected non-negative value, received %s"
+                              % type(new_amt))
+        else:
+            self._amount = new_amt
+
+    def __repr__(self):
+        if self._data:
+            return "ItemStack(%s, %s, %s)" % (self._type, self._amount, self._data)
+        return "ItemStack(%s, %s)" % (self._type, self._amount)
+    
+    def matches(self, item_type=None, exact_data=None, **fields):
+        '''check that cls and data agree with both arguments are optional'''
+        if item_type is not None and self._type is not item_type:
+            return False
+        if exact_data is not None and exact_data != self._data:
+            # edge case: this ItemStack has no data and exact_data is {}
+            # in this case exact_data are not equal but have same meaning
+            if self._data is not None or exact_data != {}:
+                return False
+        # check any remaining fields and return the result
+        return matching_subset(self._data, fields)
+
+
+# make the common case fast
+# this structure is optimized for name-based lookups
+class Inventory:
+    def __init__(self):
+        self._items = defaultdict(list)
+    
+    def add_item(self, item):
+        name = str(item)
+        item_data = item.save()
+        #TODO: convert empty dict to None to save space
+        # see if a stack of this item exists
+        for stack in self._items[name]:
+            if stack.matches(type(item), item_data):
+                stack.amount += 1
+                break
+        # otherwise, create a new stack
+        else:
+            new_stack = ItemStack(type(item), 1, item_data)
+            self._items[name].append(new_stack)
+    
+    def remove_item(self, item):
+        '''raises key error if item is not in the stack'''
+        name = str(item)
+
+    def find(self, name=None, cls=None, exact_data=None, **other_fields):
+        if name:
+            # if name provided, get the corresponding bucket
+            if name in self._items:
+                for stack in self._items[name]:
+                    if stack.matches(cls, exact_data, other_fields):
+                        yield tuple(stack)
+        else:
+            # if not, search through every bucket
+            for name, bucket in self._items.items():
+                for stack in bucket:
+                    pass
+
+
 class Inventory:
     '''Inventory for containing items, stacking by quantity
     all items are stored in _items, which follows this layout
