9d11852c3d076c0db96d54d7219c72bb1b45e4ca
xeranimus <nobodycaresaboutspam@gmail.com>
2019-07-27 12:58:12 -0400

    added to_base function

--- util/misc.py
+++ util/misc.py
@@ -10,6 +10,32 @@ def camel_to_space(name):
         output.append(letter)
     return "".join(output).strip()
 
+def to_base(num, base, alphabet="0123456789abcdefghijklmnopqrstuvwxyz"):
+    '''convert an unsigned integer [num] to base 36'''
+    # check that the base is in an appropriate range
+    try:
+        max_base = len(alphabet)
+        if base > len(alphabet) or base < 2:
+            raise ValueError("Expected base in range [2, %i], received %r"
+                             % (max_base, base))
+    except TypeError:
+        raise TypeError("Expected base with <class 'int'>, received %r" % num)
+    # check that the num is an unsigned integer
+    try:
+        if num < 1:
+            if num == 0:
+                return "0"
+            else:
+                raise ValueError("Expected unsigned integer, received %i"
+                                 % num)
+    except TypeError:
+        raise TypeError("Expected num with <class 'int'>, received %r" % num)
+    digits = []
+    while num > 0:
+        num, digit = divmod(num, base)
+        digits.append(alphabet[digit])
+    return "".join(digits[::-1])
+
 def group_and_count(items, format="%s\t[%i]", single_format="%s", sep="\n"):
     '''takes a list of items and a formatter,
     and produces a list with counts
