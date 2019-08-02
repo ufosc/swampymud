e0479c362b14acd569d3f4be886d77ce89453b2c
xeranimus <nobodycaresaboutspam@gmail.com>
2019-08-02 10:36:29 -0400

    added default command testcase

--- testing/test_character.py
+++ testing/test_character.py
@@ -1,17 +1,20 @@
 '''module testing the Character class'''
 import unittest
-import control
-import character
-import location
+import control as con
+import character as char
+import location as loc
 
-class EntryPlug(control.Controller):
+class EntryPlug(con.Controller):
     '''simple controller for programmatic use'''
     def __init__(self, character):
         super().__init__()
         self.assume_control(character)
         self.last_cmd = None
         self.msgs = []
-    
+
+    def __repr__(self):
+        return f"EntryPlug{self.receiver}"
+
     def command(self, cmd):
         '''add a command and update the receiver to respond'''
         self.last_cmd = cmd
@@ -23,15 +26,31 @@ class EntryPlug(control.Controller):
         return cmd
 
     def has_cmd(self):
+        '''returns true if command is available'''
         return self.last_cmd is not None
 
     def write_msg(self, msg):
+        '''write [msg] back to the controller'''
         self.msgs.append(msg)
     
     def has_msg(self):
+        '''returns true if messages are available'''
         return bool(self.msgs)
         
-room = location.Location("Room", "This is just a room for testing.")
-billy = character.Character("Billy")
-billy.set_location(room)
-con = EntryPlug(billy)
+TEST_ROOM = loc.Location("Room", "This is just a room for testing.")
+
+class TestDefaultCommands(unittest.TestCase):
+    
+    def setUp(self):
+        self.room = loc.Location("Room", "This is just a room for testing.")
+        self.bill = char.Character("Bill")
+        self.bill.set_location(TEST_ROOM)
+        self.billcon = EntryPlug(self.bill)
+    
+    def tearDown(self):
+        self.bill.die()
+    
+    def test_help(self):
+        '''test that help command works properly'''
+        self.billcon.command("help")
+        # check message
