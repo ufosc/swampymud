4836378dc9fb7a376c1073600ca871c0df444584
xeranimus <nobodycaresaboutspam@gmail.com>
2019-08-03 00:16:06 -0400

    added CharFilter testcase

--- testing/test_character.py
+++ testing/test_character.py
@@ -4,8 +4,233 @@ import control as con
 import character as char
 import location as loc
 
+
+# defining some test CharacterClasses
+class Human(char.Character):
+    """base class for all humans"""
+
+class Soldier(Human):
+    """a soldier class"""
+
+class Bureaucrat(Human):
+    """a different kind of human class"""
+
+class Commander(Soldier, Bureaucrat):
+    """inherits from both soldier and bureaucrat"""
+
+class Slug(char.Character):
+    """a non-human base class"""
+
+class AlphaSlug(Slug):
+    """a class derived from Slug"""
+
+class TestCharFilter(unittest.TestCase):
+    """basic testcase for the pervasive CharFilter class"""
+
+    # defining several CharacterClasses for testing
+
+    def setUp(self):
+        self.maxDiff = 1000
+        # a boring human
+        self.gary = Human()
+        # some soldiers
+        self.chad = Soldier()
+        self.zeke = Soldier()
+        # a bureaucrat
+        self.bill = Bureaucrat()
+        # a commander
+        self.dwight = Commander()
+        # simple slugs
+        self.bloog = Slug()
+        self.plubb = Slug()
+        # an intimidating alpha slug
+        self.vloobuk = AlphaSlug()
+        # lists of each character / CharacterClass to avoid repetition
+        self.char_list = [
+            self.gary, self.chad, self.zeke, self.bill, self.dwight,
+            self.bloog, self.plubb, self.vloobuk
+        ]
+        self.classes = [
+            Human, Soldier, Bureaucrat, Commander, Slug, AlphaSlug
+        ]
+        # some typical test results
+        self.all_chars = {
+            char: True for char in self.char_list + self.classes
+        }
+        self.no_chars = {
+            char: False for char in self.char_list + self.classes
+        }
+
+    def filter_test(self, filt):
+        """helper function that returns a dict mapping Characters to a boolean
+        representing whether or not [filt] allows them through
+        """
+        return {
+            char: filt.permits(char) for char in self.char_list + self.classes
+        }
+
+    def test_repr(self):
+        """test that repr(CharFilter) works as expected"""
+        filt = char.CharFilter(mode=False)
+        self.assertEqual(repr(filt),
+                         "CharFilter(False, set(), set(), set())")
+        filt = char.CharFilter(mode=True)
+        self.assertEqual(repr(filt),
+                         "CharFilter(True, set(), set(), set())")
+        filt = char.CharFilter(False, [Slug, AlphaSlug])
+        # testing further is difficult due to set order changing
+
+    def test_mode(self):
+        """test that __init__'s mode keyword works as expected"""
+        # creating a whitelist and blacklist with strings
+        whitelist = char.CharFilter(mode="whitelist")
+        blacklist = char.CharFilter(mode="blacklist")
+        self.assertEqual(self.filter_test(whitelist), self.no_chars)
+        self.assertEqual(self.filter_test(blacklist), self.all_chars)
+        # creating a whitelist and blacklist with booleans
+        whitelist = char.CharFilter(mode=True)
+        blacklist = char.CharFilter(mode=False)
+        self.assertEqual(self.filter_test(whitelist), self.no_chars)
+        self.assertEqual(self.filter_test(blacklist), self.all_chars)
+        # creating a whitelist and blacklist with enum values
+        whitelist = char.CharFilter(char.FilterMode.WHITELIST)
+        blacklist = char.CharFilter(char.FilterMode.BLACKLIST)
+        self.assertEqual(self.filter_test(whitelist), self.no_chars)
+        self.assertEqual(self.filter_test(blacklist), self.all_chars)
+
+    def test_include_classes(self):
+        """test that including CharacterClasses allows them through"""
+        whitelist = char.CharFilter(mode="whitelist")
+        whitelist.include(AlphaSlug)
+        # only AlphaSlugs are allowed through
+        expected = self.no_chars.copy()
+        expected[self.vloobuk] = True
+        expected[AlphaSlug] = True
+        self.assertEqual(self.filter_test(whitelist), expected)
+        # test that AlphaSlug was added to the set
+        self.assertEqual(whitelist._classes, set([AlphaSlug]))
+        # now all soldier and soldier-derived classes should be permited
+        whitelist.include(Soldier)
+        expected[self.chad] = True
+        expected[self.zeke] = True
+        expected[self.dwight] = True
+        expected[Soldier] = True
+        expected[Commander] = True
+        self.assertEqual(self.filter_test(whitelist), expected)
+        # test that Soldier was added to the set
+        self.assertEqual(whitelist._classes, set([AlphaSlug, Soldier]))
+
+        # now test including with a blacklist
+        blacklist = char.CharFilter(mode="blacklist")
+        blacklist.include(AlphaSlug)
+        expected = self.all_chars.copy()
+        # it's a blacklist, and nobody was excluded, so all are permitted
+        self.assertEqual(self.filter_test(blacklist), expected)
+        # no classes should be added to set
+        self.assertEqual(blacklist._classes, set())
+
+    def test_exclude_classes(self):
+        """test that excluding CharacterClasses does not allow them through"""
+        whitelist = char.CharFilter(mode="whitelist")
+        # excluding a class from a whitelist should have no effect
+        # since whitelists only track included classes
+        whitelist.exclude(Human)
+        expected = self.no_chars.copy()
+        self.assertEqual(self.filter_test(whitelist), expected)
+        # no classes should be added to set
+        self.assertEqual(whitelist._classes, set())
+
+        # now testing a blacklist
+        blacklist = char.CharFilter(mode="blacklist")
+        blacklist.exclude(AlphaSlug)
+        expected = self.all_chars
+        expected[self.vloobuk] = False
+        expected[AlphaSlug] = False
+        self.assertEqual(self.filter_test(blacklist), expected)
+        # AlphaSlug should be added to set
+        self.assertEqual(blacklist._classes, set([AlphaSlug]))
+        # excluding another class
+        blacklist.exclude(Bureaucrat)
+        expected[self.bill] = False
+        expected[self.dwight] = False
+        expected[Bureaucrat] = False
+        expected[Commander] = False
+        self.assertEqual(self.filter_test(blacklist), expected)
+        # Bureaucrat should be added to set
+        self.assertEqual(blacklist._classes, set([AlphaSlug, Bureaucrat]))
+
+    def test_whitelist_chars(self):
+        """including/excluding Characters should overrides any class
+        permissions for whitelists"""
+        whitelist = char.CharFilter(mode="whitelist")
+        whitelist.include(Soldier)
+        whitelist.exclude(self.dwight)
+        expected = self.no_chars.copy()
+        # add all soldiers, except Dwight who we excluded
+        expected[self.chad] = True
+        expected[self.zeke] = True
+        expected[Soldier] = True
+        expected[Commander] = True
+        self.assertEqual(self.filter_test(whitelist), expected)
+        # check that the classes and include chars work as expected
+        self.assertEqual(whitelist._classes, set([Soldier]))
+        self.assertEqual(whitelist._include_chars, set())
+        self.assertEqual(whitelist._exclude_chars, set([self.dwight]))
+        # now include dwight
+        whitelist.include(self.dwight)
+        expected[self.dwight] = True
+        self.assertEqual(self.filter_test(whitelist), expected)
+        self.assertEqual(whitelist._classes, set([Soldier]))
+        self.assertEqual(whitelist._include_chars, set([self.dwight]))
+        self.assertEqual(whitelist._exclude_chars, set())
+        # now exclude the Soldier class
+        whitelist.exclude(Soldier)
+        expected = self.no_chars.copy()
+        expected[self.dwight] = True
+        self.assertEqual(self.filter_test(whitelist), expected)
+        self.assertEqual(whitelist._classes, set())
+        self.assertEqual(whitelist._include_chars, set([self.dwight]))
+        self.assertEqual(whitelist._exclude_chars, set())
+
+    def test_blacklist_chars(self):
+        """including/excluding Characters should overrides any class
+        permissions for blacklists"""
+        blacklist = char.CharFilter(mode="blacklist")
+        # exclude Slug in general, but include bloog
+        blacklist.exclude(Slug)
+        blacklist.exclude(self.vloobuk)
+        blacklist.include(self.bloog)
+        expected = self.all_chars.copy()
+        expected[Slug] = False
+        expected[AlphaSlug] = False
+        expected[self.vloobuk] = False
+        expected[self.plubb] = False
+        self.assertEqual(self.filter_test(blacklist), expected)
+        self.assertEqual(blacklist._classes, set([Slug]))
+        self.assertEqual(blacklist._include_chars, set([self.bloog]))
+        self.assertEqual(blacklist._exclude_chars, set([self.vloobuk]))
+        # now include the Slug class in general again
+        blacklist.include(Slug)
+        expected[Slug] = True
+        expected[AlphaSlug] = True
+        expected[self.plubb] = True
+        self.assertEqual(self.filter_test(blacklist), expected)
+        self.assertEqual(blacklist._classes, set())
+        self.assertEqual(blacklist._include_chars, set([self.bloog]))
+        self.assertEqual(blacklist._exclude_chars, set([self.vloobuk]))
+        # now include vloobuk and exclude bloog
+        blacklist.include(self.vloobuk)
+        blacklist.exclude(self.bloog)
+        expected[self.vloobuk] = True
+        expected[self.bloog] = False
+        self.assertEqual(self.filter_test(blacklist), expected)
+        self.assertEqual(blacklist._classes, set())
+        self.assertEqual(blacklist._include_chars, set([self.vloobuk]))
+        self.assertEqual(blacklist._exclude_chars, set([self.bloog]))
+
+
 class EntryPlug(con.Controller):
-    """simple controller for programmatic use"""
+    """simple controller for programmatic control of Characters"""
     def __init__(self, character):
         super().__init__()
         self.assume_control(character)
@@ -36,7 +261,7 @@ class EntryPlug(con.Controller):
     def has_msg(self):
         """returns true if messages are available"""
         return bool(self.msgs)
-        
+
 TEST_ROOM = loc.Location("Room", "This is just a room for testing.")
 TEST_OUT = loc.Location("Outside", "This room is outside.")
 TEST_EXIT = loc.Exit(TEST_OUT, "out", ["outside", "test out"])
@@ -127,7 +352,7 @@ class TestDefaultCommands(unittest.TestCase):
         self.billcon.command("go foobar")
         self.assertEqual(self.billcon.msgs, ["No exit with name 'foobar'."])
         self.assertTrue(self.bill.location is TEST_ROOM)
-    
+
     def test_go_basic(self):
         """test that basic use of the go command works properly"""
         self.billcon.command("go outside")
