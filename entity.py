584045393a53c4918c8c85f0a110a70c5f0f34a9
wsowens <wowens@ufl.edu>
2020-05-06 12:59:49 -0400

    added basic testcases for entity

--- entity.py
+++ entity.py
@@ -1,102 +1,59 @@
-'''Method defining an entity'''
-import character
+'''Module defining the entity class'''
 from util import camel_to_space
+import character
+import inspect
+
 
 class EntityClass(type):
-    '''metaclass controlling entity types'''
+    '''Metaclass controlling entity types'''
 
-    # TODO: move dict of individual entities into the server
     def __init__(self, cls, bases, namespace):
         '''initialize an Entity class'''
-        super().__init__(cls, bases, namespace)
         if "classname" not in namespace:
             self.classname = camel_to_space(cls)
-        self._instances = {}
-        self._nextid = 0
+        if "description" not in namespace:
+            if self.__doc__ is not None:
+                self.description = inspect.cleandoc(self.__doc__)
+            else:
+                self.description = "[No description provided.]"
+
+        # note that this is essentially the same as CharacterClass
+        # first gather the Commands defined in this class
+        self._local_commands = {}
+        for value in namespace.values():
+            if isinstance(value, character.Command):
+                value.label = "Equipped"
+                self._local_commands[str(value)] = value
+
+        # now gather all commands, with the most recent commands exposed
         self._commands = {}
-        for item in namespace.values():
-            if isinstance(item, character.Command):
-                self._commands[item.name] = item
+        for base in reversed(self.__mro__):
+            if not isinstance(base, EntityClass):
+                continue
+            self._commands.update(base._local_commands)
+        self._commands.update(self._local_commands)
 
-        entity_bases = list(filter(lambda x: isinstance(x, EntityClass),
-                                   self.__mro__))
-        # build the set of command names / commands by looking through
-        # the mro
-        # there is probably a better way to do this
-        for base in entity_bases:
-            for cmd in base._commands.values():
-                if cmd.name not in self._commands:
-                    self._commands[cmd.name] = cmd
-
-
-    def __getitem__(self, key):
-        '''overriding cls[k]'''
-        return self._instances[key]
-
-    def __setitem__(self, key, item):
-        ''' overriding cls[k] = item'''
-        self._instances[key] = item
-
-    def __delitem__(self, key):
-        '''overriding del cls[k]'''
-        del self._instances[key]
-
-    def __str__(self):
-        '''overriding str()'''
-        return self.classname
-
-    def describe(self):
-        ''' Describes the entity '''
-        return str(self)
-
-    def cmd_name_set(cls, char=None):
-        '''returns a set of the names for each command'''
-        if char is None:
-            return set(cls._commands)
-        else:
-            nameset = set()
-            for cmd in cls._commands.values():
-                if cmd.filter.permits(char):
-                    nameset.add(cmd.name)
-            return nameset
-
-    def intersect(cls, other_entity, char=None):
-        '''returns the set of names that have corresponding
-        commands in both classes
-        if a char is provided, only those commands that the
-        character can use are included'''
-        if other_entity is cls:
-            return cls.cmd_name_set(char)
-        names = cls.cmd_name_set(char)
-        other_names = other_entity.cmd_name_set(char)
-        return names & other_names
+    def __str__(cls):
+        '''returns a string representation of this class'''
+        return cls.classname
 
 
 class Entity(metaclass=EntityClass):
-
-    #TODO: consider changing this "proper_name" thing?
-    def __init__(self, proper_name=None):
+    '''Base class for all other entities'''
+    def __init__(self):
         self.location = None
-        self.proper_name = proper_name
-        self._id = self._nextid
-        self.__class__._nextid += 1
-        self._instances[self._id] = self
-        self._isNPC = False
-
-    def __repr__(self):
-        return "%s[%i]" % (type(self).__name__, self._id)
 
     def __str__(self):
-        if self.proper_name is not None:
-            return self.proper_name
-        else:
-            return repr(self)
+        '''Return a simple representation of this item.
+        By default, str(item) returns the name of the entity's class.
+        '''
+        return self.classname
 
     def set_location(self, new_location):
         '''sets location, updating previous location as appropriate'''
         try:
             self.location.entities.remove(self)
-            # remove this entity's commands from all the 
+            # remove this entity's commands from all the
             # characters in the current location
             for char in self.location.characters:
                 self.remove_cmds(char)
@@ -112,41 +69,41 @@ class Entity(metaclass=EntityClass):
 
     def add_cmds(self, char):
         '''add a command to a character'''
-        # first, determine the commands that collide with other
-        # entities commands
-        collided_cmds = set()
-        for entity in self.location.entities:
-            if entity is not self:
-                collided_cmds.update(type(self).intersect(type(entity), char))
         for cmd in self._commands.values():
-            cmd = cmd.specify(self, char)
-            # only add the command if the filter permits the char
             if cmd.filter.permits(char):
-                # if the command could be ambigious, 
-                # supply the entity name to the command
-                if cmd.name in collided_cmds:
-                    char.cmd_dict.add_cmd(cmd, name=cmd.name + "-%s" % str(self))
-                else:
-                    char.cmd_dict.add_cmd(cmd)
+                cmd = cmd.specify(self, char)
+                char.new_cmd_dict[str(cmd)] = cmd
 
     def remove_cmds(self, char):
-        '''remove all commands from this char that
-        belong to this entity'''
+        '''remove all commands from this char from this entity'''
         for cmd in self._commands.values():
             cmd = cmd.specify(self, char)
-            if char.cmd_dict.has_cmd(cmd):
-                char.cmd_dict.remove_cmd(cmd)
+            try:
+                char.new_cmd_dict.remove_value(str(cmd), cmd)
+            # command was not in cmd_dict
+            except KeyError:
+                pass
+            except ValueError:
+                pass
 
-    @property
-    def isNPC(self):
-        return self._isNPC
+    # these methods can be overriden
+    def on_enter(self, char: character.Character):
+        pass
 
+    def on_exit(self, char: character.Character):
+        pass
+
+    def on_say(self, char: character.Character, msg: str):
+        pass
+
+    # serialization-related methods
     @classmethod
     def load(cls, data):
         proper_name = None
-        if "proper_name" in data:
-            proper_name = data["proper_name"]
         return cls(proper_name)
 
     def postload(self, data, obj_symbols, cls_symbols):
         pass
+
+    def save(self):
+        return {}
