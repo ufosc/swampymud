8d3b3b0b62cf069b2e9b24ff163ae973138d3564
wsowens <wowens@ufl.edu>
2020-10-05 03:22:00 -0400

    added parser for user-provided grammars

--- swampymud/util/parser.py
+++ swampymud/util/parser.py
@@ -1,6 +1,12 @@
 """module for parser generator
 The Grammar of Command Grammars:
-grammar =
+(whitespace is ignored)
+grammar = expression+
+expression = '(' atom  ('|' atom )* ')' quantifer?
+atom = identifier | keyword
+quantifier = '?' | '*' | '+'
+identifier = [A-Z]+
+keyword = [A-Z]+
 
 
 Technically speaking, parsing player input is *NOT* context free.
@@ -20,10 +26,23 @@ Or is the user just trying to take the treasure guardian (invalid).
 This means that many of the typical parser-generator strategies don't
 work here.
 
-In the grammar of these paraser-generators, we note the following.
+In the grammar of these paraser-generators, we note the following:
+"objects" are phrases that should match in-game objects. They are indicated
+in parentheses, with the type indicated all caps.
+pickup (ITEM)
+If more than one type is accepted, you can specify like so:
+attack (ITEM | ENTITY)
+You can provide multiple types (if desired):
+attack (ITEM)
 
+"keywords" are constant literal phrases. Most often, they are prepositions,
+like the 'to' in the example below:
+    give (ITEM) to (CHARACTER | ENTITY)
 
+Parsing a list of tokens:
+    work through the
 """
+import re
 
 def split_args(string):
     """Split [string] on whitespace, respecting quotes.
@@ -70,3 +89,183 @@ def split_args(string):
     # TODO: should we catch if in_quote is still open?
     return ["".join(token) for token in tokens]
 
+
+def string_index(tok_index, tokens):
+    # compute the cumulative length of the list of tokens up to token [index]
+    cumsum = []
+    prev = 0
+    for s in tokens[:tok_index]:
+        length = len(s) if s else 0
+        cumsum.append(length + prev)
+        prev = length
+    if cumsum:
+        return cumsum[-1]
+    # edge case: provided index is 0
+    else:
+        return 0
+
+
+_grammar_token_re = re.compile(r"([()|*?])|[ \t\r\n]")
+def with_grammar(grammar: str):
+    """Returns a parser based on the provided grammar."""
+    # actually, it just returns a DFA
+    # parse grammar into tokens
+    tokens = _grammar_token_re.split(grammar)
+
+    # stack of grammar components
+    stack = [Group()]
+
+    for tok_index, token in enumerate(tokens):
+        if not token:
+            # token is empty string or None (whitespace)
+            continue
+        if token.islower():
+            # it's a keyword
+            stack[-1].add(Keyword(token))
+        elif token.isupper():
+            # do some kind of type checking here
+            stack[-1].add(Variable(token))
+        elif token == "(":
+            # start new capturing group
+            stack.append(Group())
+        elif token == ")":
+            # end previous capturing group / union
+            finished_group = stack.pop()
+            finished_group.cleanup()
+            if stack:
+                stack[-1].add(finished_group)
+            else:
+                index = string_index(tok_index, tokens)
+                raise ValueError(f"Unmatched ')' at index [{index}]")
+        elif token == "|":
+            stack[-1].add_alternative()
+        # quantifiers
+        elif token == "*":
+            stack[-1].quantify_last(Star)
+        elif token == "+":
+            stack[-1].quantify_last(Plus)
+        elif token == "?":
+            stack[-1].quantify_last(Optional)
+        else:
+            index = string_index(tok_index, tokens)
+            raise ValueError(f"Unrecognized token {token!r} starting at index [{index}]")
+    # do we have any unfinished capturing groups on the stack?
+    if len(stack) > 1:
+        count = len(stack) - 1
+        index = string_index(0, tokens)
+        raise ValueError(f"Expected {count} ')', but input ended at [{index}]")
+    stack[0].cleanup()
+    return stack[0]
+
+class Matcher:
+    """A grammar is implicitly concatentation"""
+    def __repr__(self):
+        return f"{type(self).__name__}({self.args!r})"
+
+class Group:
+    """Implictly, a group functions as a stack within our stack"""
+    def __init__(self, *args, alts=None):
+        alts = [] if alts is None else alts
+        self.alts = list(alts)
+        self.args = list(args)
+
+    def add(self, arg):
+        self.args.append(arg)
+        return self
+
+    def quantify_last(self, Quantifier):
+        if self.args:
+            if not isinstance(self.args[-1], (Star, Plus, Optional)):
+                self.args[-1] = Quantifier(self.args[-1])
+            else:
+                raise ValueError("Predicate already has quantifier")
+        else:
+            raise ValueError("Expected predicate before quantifier")
+
+    def add_alternative(self):
+        if not self.args:
+            # if we haven't added anything, raise an error
+            raise ValueError("empty alternative")
+        self.alts.append(self.args)
+        self.args = []
+
+    def __repr__(self):
+        if self.alts:
+            if self.args:
+                return f"Group({repr(self.args)[1:-1]}, alts={self.alts})"
+            else:
+                return f"Group(alts={self.alts})"
+        else:
+            return f"Group({repr(self.args)[1:-1]})"
+
+    def cleanup(self):
+        """This operation detects
+        1) any empty alternates
+        2) empty group
+        It also closed out the most recent alternative and adds it to
+        the stack
+        """
+        if not self.args:
+            if self.alts:
+                # the most recent alternate is empty
+                raise ValueError("empty alternate")
+            else:
+                # the entire group is empty
+                raise ValueError("empty group")
+        else:
+            if self.alts:
+                self.alts.append(self.args)
+                self.args = []
+
+
+# two types of basic grammar expressions
+class Keyword(Matcher):
+    def __init__(self, kw):
+        self.args = kw
+
+class Variable(Matcher):
+    #TODO: possible optimization, join types in an alternate
+    # together into one variable
+    def __init__(self, *types):
+        self.args = types
+
+# three quantifiers
+class Star(Matcher):
+    def __init__(self, expr):
+        self.args = expr
+
+class Plus(Matcher):
+    def __init__(self, expr):
+        self.args = expr
+
+class Optional(Matcher):
+    def __init__(self, expr):
+        self. args = expr
+
+
+"""
+Prologue:
+Worst thing that can happen is "token contention". In other words,
+two variables fighting over a phrase. Say that our grammar is
+"attack (ENTITY) (ITEM)"
+and we get the input:
+"attack evil zombie sword"
+
+
+Workflow:
+Parser:
+Run the tokens through the matcher / finite-state automaton. (Each
+grammar resolves to exactly one FSA).
+This produces one or more valid paths.
+A path is valid if the automaton ends in the FINAL state.
+If this occurs, then all of the tokens should be associated with one
+or more tokens.
+
+Each path is then run through the "contextualizer".
+The contextualizer makes heavy use of util.find to match objects to the
+phrases.
+If
+"""
+class Matcher:
+    """Abstact Base Class for finite state automata"""
+    pass
--- tests/test_util_parser.py
+++ tests/test_util_parser.py
@@ -1,5 +1,6 @@
 import unittest
 from swampymud.util import parser
+from swampymud.util.parser import Keyword, Variable, Group, Star, Plus, Optional
 
 class TestParser(unittest.TestCase):
 
@@ -45,3 +46,41 @@ class TestParser(unittest.TestCase):
                 ["say", "who is that?"])
         compare('hey whoops I "forgot to close this quote',
                 ["hey", "whoops", "I", "forgot to close this quote"])
+
+
+    def gammar_parser(self):
+        # convenience function
+        def compare(inp, output):
+            self.assertEqual(parser.with_grammar(inp), output)
+        compare("foo", Group(Keyword("foo")))
+        compare("foo bar", Group(Keyword("foo bar")))
+        compare("foo (bar (baz) bok)",
+                Group(Keyword("foo"),
+                      Group(Keyword("bar"),
+                            Group(Keyword("baz")),
+                            Keyword("Bok"))))
+        compare("foo | bar",
+                Group(alts=[[Keyword("foo")], [Keyword("bar")]]))
+        compare("foo | ITEM",
+                Group(alts=[[Keyword("foo")], [Variable("ITEM")]]))
+        compare("foo?",
+                Group(Optional("foo")))
+        compare("foo*",
+                Group(Star("foo")))
+        compare("foo+",
+                Group(Plus("foo")))
+        compare("give ITEM to? ( ENTITY | CHARACTER )",
+                Group(Keyword("give"),
+                      Variable("ITEM"),
+                      Optional(Keyword("to")),
+                      Group(alts=[[Variable("ENTITY")],
+                                  [Variable("CHARACTER")]])))
+
+        compare("put (ENTITY | ITEM down?)",
+                Group(Keyword("put"),
+                      Group(alts=[
+                          [Variable("ENTITY")],
+                          [Variable("Item"), Optional(Keyword("down"))]
+                      ])))
+
+        # TODO: test all the errors and make a better exception
